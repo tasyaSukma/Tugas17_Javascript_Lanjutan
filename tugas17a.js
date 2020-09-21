14ae2208d2971d519e4c5787fab067be6ad1025a
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-21 09:36:21 +0700

    Add files via upload

---
+++ tugas17a.js
@@ -0,0 +1,7 @@
+var txt = "Saya beLajar bahaSa peMrograman deNgan khUsuk"
+var txt2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+console.log(txt.toLowerCase())
+console.log(txt.toUpperCase())
+console.log(txt2.toLowerCase())
+console.log(txt2.toUpperCase())
